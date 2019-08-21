afb728ae1e0dfd7305295a83b1b44b72ebe31a0d
Harshita Sahai <40337803+HarshitaSahai@users.noreply.github.com>
2019-08-21 20:22:23 +0530

    Create zigzag.cpp

---
+++ zigzag.cpp
@@ -0,0 +1,53 @@
+//check for even and odd places
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long int n,i;
+	    cin>>n;
+	    
+	    long int a[n];
+	    for(i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+	    
+	    for(i=0;i<n-1;i++)
+	    {
+	        if(i%2==0)
+	        {
+	            if(a[i]>a[i+1])
+	            {
+	                int temp;
+                    temp=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
+	            }
+	            
+	        }
+	        else if(a[i+1]>a[i])
+	        {
+	            int temp;
+                    temp=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
+	        }
+	    }
+	    
+	    for(i=0;i<n;i++)
+	    {
+	        cout<<a[i]<<" ";
+	    }
+	    
+	    cout<<endl;
+	}
+	return 0;
+}
