4350499303101bb09de99a4291e69c72f5f0e776
Harshita Sahai <40337803+HarshitaSahai@users.noreply.github.com>
2019-08-26 00:48:23 +0530

    Create reversequeue.cpp

---
+++ reversequeue.cpp
@@ -0,0 +1,46 @@
+
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+queue<long long int> rev(queue<long long int> q);
+int main()
+{
+    long long int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<long long int> q; 
+    long long int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    queue<long long int> a=rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+}
+/*This is a function problem.You only need to complete the function given below*/
+//function Template for C++
+queue<long long int> rev(queue<long long int> q)
+{
+    stack<int> s ;
+    while(!q.empty())
+    {
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+    }
+    return q;
+}
